0c02140098b2130ded22db66a906761433a636fb
VADIVEL007-max <vadivels759@gmail.com>
2026-07-02 22:40:23 +0530

    add axios interceptor for automatic JWT authentication

--- ecommerce-frontend/src/services/api.js
+++ ecommerce-frontend/src/services/api.js
@@ -1,7 +1,24 @@
+
+
 import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://ecom-backend-nmzd.onrender.com/",
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
