38effb3cfc94743cc0956e0cd3e7bd9ff52eccdc
VADIVEL007-max <vadivels759@gmail.com>
2026-07-02 23:05:54 +0530

    remove manual auth headers from cart service

--- ecommerce-frontend/src/services/cartService.js
+++ ecommerce-frontend/src/services/cartService.js
@@ -1,65 +1,22 @@
+
 import api from "./api";
 
 // GET Cart
-export const getCart = async () => {
-  const token = localStorage.getItem("accessToken");
-  return api.get("/api/cart", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export const getCart = () => {
+  return api.get("/api/cart");
 };
 
 // ADD To Cart
-export const addToCart = async (data) => {
-  const token = localStorage.getItem("accessToken");
-  return api.post("/api/cart", data, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export const addToCart = (data) => {
+  return api.post("/api/cart", data);
 };
 
 // UPDATE Cart
-export const updateCart = async (id, data) => {
-  const token = localStorage.getItem("accessToken");
-  return api.put(`/api/cart/${id}`, data, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export const updateCart = (id, data) => {
+  return api.put(`/api/cart/${id}`, data);
 };
 
 // DELETE Cart
-export const deleteCart = async (id) => {
-  const token = localStorage.getItem("accessToken");
-  return api.delete(`/api/cart/${id}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export const deleteCart = (id) => {
+  return api.delete(`/api/cart/${id}`);
 };
-
-
-
-// import api from "./api";
-
-// // GET Cart
-// export const getCart = () => {
-//   return api.get("/api/cart");
-// };
-
-// // ADD To Cart
-// export const addToCart = (data) => {
-//   return api.post("/api/cart", data);
-// };
-
-// // UPDATE Cart
-// export const updateCart = (id, data) => {
-//   return api.put(`/api/cart/${id}`, data);
-// };
-
-// // DELETE Cart
-// export const deleteCart = (id) => {
-//   return api.delete(`/api/cart/${id}`);
-// };
